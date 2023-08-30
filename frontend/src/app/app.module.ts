d22ad7b1332c23e6713cf1f023718b073726db2e
Anthony Schneider <tonyschneider3@gmail.com>
2023-08-30 18:40:08 -0500

    added theme + a few basic components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {importProvidersFrom} from '@angular/core';
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {VERSION as MAT_VERSION, MatNativeDateModule} from '@angular/material/core';
+
 import { AppComponent } from './app.component';
+import { TableComponent } from '../components/table/table.component';
+import { InputClearable } from '../components/input-clearable/input-clearable.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    InputClearable,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(MatNativeDateModule),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
