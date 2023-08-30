b3120cace4afa5924b73ec1fd8865ddd0cf1e21f
Anthony Schneider <tonyschneider3@gmail.com>
2023-08-29 20:56:17 -0500

    added create item API endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get-data/', views.get_data, name='get_data'),
+    path('create-item/', views.create_item, name='create_item'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Item
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 
 # Create your views here.
 
@@ -10,3 +13,27 @@ def get_data(request):
         {'id': 3, 'name': 'Item 3'},
     ]
     return JsonResponse(data, safe=False)
+
+
+def create_item(request):
+    name = request.POST.get('name')
+    description = request.POST.get('description')
+    price = request.POST.get('price')
+
+    item = Item(name=name, description=description, price=price)
+    item.save()
+
+    return JsonResponse({"message": "Item created successfully"})
+
+
+
+# @api_view(['POST'])
+# def create_item(request):
+#     name = request.data.get('name')
+#     description = request.data.get('description')
+#     price = request.data.get('price')
+
+#     item = Item(name=name, description=description, price=price)
+#     item.save()
+
+#     return Response({"message": "Item created successfully"})
