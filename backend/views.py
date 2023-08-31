a55724dcc4fb739c6089af1860dd1e02922a3cad
Anthony Schneider <tonyschneider3@gmail.com>
2023-08-30 20:03:37 -0500

    made data match type from backend

--- backend/views.py
+++ backend/views.py
@@ -10,9 +10,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 def get_dummy_data(request):
     data = [
-        {'id': 1, 'name': 'Item 1'},
-        {'id': 2, 'name': 'Item 2'},
-        {'id': 3, 'name': 'Item 3'},
+        {'id': 1, 'name': 'Item 1', 'description': 'This is item 1', 'price': 10},
+        {'id': 2, 'name': 'Item 2', 'description': 'This is item 2', 'price': 20},
+        {'id': 3, 'name': 'Item 3', 'description': 'This is item 3', 'price': 30},
     ]
     return JsonResponse(data, safe=False)
 
