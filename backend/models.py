ff306deefec1cbc9f9624dfeb47b769fa828bfd6
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-15 18:50:19 -0500

    new: Ingredient ItemWithIngredients ItemIngredient

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
-# Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='ingredients')
+
+class ItemWithIngredients(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    ingredients = models.ManyToManyField(Ingredient, through='ItemIngredient', related_name='items')
+
+class ItemIngredient(models.Model):
+    item = models.ForeignKey(ItemWithIngredients, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    mass = models.DecimalField(max_digits=10, decimal_places=2)
