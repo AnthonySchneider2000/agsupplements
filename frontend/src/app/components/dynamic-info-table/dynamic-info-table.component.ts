7ff2b6a1b5281ce734aca39476410bccc53b91d0
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-03 20:43:04 -0500

    BUGFIX: prevent rowclick from always opening page

--- frontend/src/app/components/dynamic-info-table/dynamic-info-table.component.ts
+++ frontend/src/app/components/dynamic-info-table/dynamic-info-table.component.ts
@@ -109,20 +109,18 @@ export class DynamicInfoTableComponent implements OnInit {
 
   }
   onRowClick(row: any, event: MouseEvent) {
-    this.dataService.fetchItemById(row.id).subscribe((data) => {
-      // fetch the item data from the backend
-      this.tableDataService.setSelectedItem(data); // set the selected item in the TableDataService
-    });
     if (event.ctrlKey) {
       window.open(row.link, '_blank');
     } else if(event.shiftKey) {
+      this.dataService.fetchItemById(row.id).subscribe((data) => {
+        // fetch the item data from the backend
+        this.tableDataService.setSelectedItem(data); // set the selected item in the TableDataService
+      });
       this.tableDataService.setSelectedId(row.id);
+    }else{
+      localStorage.setItem('selectedId', row.id.toString()); // save the selected id to localStorage
+      window.location.href = '/product-page'; // navigate the current page to /product-page
     }
-    // save the selected id to localStorage
-    localStorage.setItem('selectedId', row.id.toString());
-
-    // naviagate the current page to /product-page
-    window.location.href = '/product-page';
   }
   capitalizeFirstLetter(string: string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
