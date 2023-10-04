5a1853fea2f6bb0c087f6b22b43b308000bec958
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-03 20:15:34 -0500

    render item info on product-page

--- frontend/src/app/pages/product-page/product-page.component.ts
+++ frontend/src/app/pages/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { ItemWithIngredients as Item } from 'src/app/services/models.service';
 
 @Component({
   selector: 'app-product-page',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent {
+  item: Item = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+    link: '',
+    ingredients: [],
+  };
+
+
+  constructor(
+    private dataService: DataService,
+  ) {
+    // load id from local storage
+    this.item.id = parseInt(localStorage.getItem('selectedId') || '0');
+    // load item from database
+    if(this.item.id != 0) {
+      this.getItemData();
+    }
+  }
+
+  getItemData() {
+    this.dataService.fetchItemById(this.item.id).subscribe((item) => {
+      this.item = item;
+    });
+  }
+  
+
 
 }
