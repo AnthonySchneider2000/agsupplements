ada03d8222c4469b543e7f05ec82992658a26a64
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-10 16:27:06 -0500

    added defs to each model

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,9 @@ class Ingredient(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     units = models.CharField(max_length=100, blank=True) # blank=True allows the field to be blank
+    
+    def __str__(self):
+        return self.name
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
@@ -15,15 +18,24 @@ class Item(models.Model):
     ingredients = models.ManyToManyField(Ingredient, through='ItemIngredient', related_name='items')
     tags = models.ManyToManyField('Tag', related_name='items')
     
+    def __str__(self):
+        return self.name
+    
 
 class ItemIngredient(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     mass = models.DecimalField(max_digits=10, decimal_places=2)
     
+    def __str__(self):
+        return "ItemIngredient: " + self.item.name + ":" + self.ingredient.name
+    
 class BlacklistedItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     reason = models.TextField(blank=True)
+    
+    def __str__(self):
+        return "BlacklistedItem: " + self.item.name + ":" + self.reason
     
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
